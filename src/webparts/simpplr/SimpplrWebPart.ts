e0b035bd0e99d4accca69acb19646233e0b73077
Uday-Bellamkonda <ubellamkonda@ornlfcu.com>
2026-09-05 11:59:39 -0500

    initial commit

---
+++ src/webparts/simpplr/SimpplrWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'SimpplrWebPartStrings';
+import Simpplr from './components/Simpplr';
+import { ISimpplrProps } from './components/ISimpplrProps';
+
+export interface ISimpplrWebPartProps {
+  description: string;
+}
+
+export default class SimpplrWebPart extends BaseClientSideWebPart<ISimpplrWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ISimpplrProps> = React.createElement(
+      Simpplr,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/simpplr/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISimpplrWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SimpplrWebPartStrings' {
+  const strings: ISimpplrWebPartStrings;
+  export = strings;
+}
